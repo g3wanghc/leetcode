b175b23b2cbd61e457747885c12743f93dda7cd0
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-05-13 15:30:15 -0400

    Syntax change

--- 002_add_two_numbers/add_two_numbers.go
+++ 002_add_two_numbers/add_two_numbers.go
@@ -16,17 +16,17 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) ListNode {
    
     var carry_over bool = false
     
-    for (*l1_node) != nil || (*l2_node) != nil || carry_over {
+    for *l1_node != nil || *l2_node != nil || carry_over {
         var sum int = 0
         
-        if (*l1_node) != nil{
+        if *l1_node != nil{
             sum += (*l1_node).Val
         }
-        
-        if (*l2_node) != nil{
+
+        if *l2_node != nil{
             sum += (*l2_node).Val
         }
-        
+
         if carry_over {
             sum += 1
             carry_over = false
@@ -36,19 +36,19 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) ListNode {
             sum = sum % 10
             carry_over = true
         } 
-        
-        if (*l1_node) != nil{
+
+        if *l1_node != nil{
             l1_node = &(*l1_node).Next
         }
         
-        if (*l2_node) != nil{
+        if *l2_node != nil{
             l2_node = &(*l2_node).Next
         }
         
         if !l3_empty{
             var tmp ListNode = ListNode {sum, nil}
-            (*l3_node).Next = &tmp
-            l3_node = (*l3_node).Next
+            l3_node.Next = &tmp
+            l3_node = l3_node.Next
         } else {
             l3 = ListNode {sum, nil}
             l3_empty = false
