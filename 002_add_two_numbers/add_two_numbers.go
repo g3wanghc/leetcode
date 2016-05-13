eaafa9f6cdab1daf6d8b16985f36f3717d60ba2f
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-05-13 16:01:08 -0400

    Fix pointers

--- 002_add_two_numbers/add_two_numbers.go
+++ 002_add_two_numbers/add_two_numbers.go
@@ -7,8 +7,8 @@
  */
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) ListNode {
-    var l1_node = &l1
-    var l2_node = &l2
+    var l1_node = l1
+    var l2_node = l2
     
     var l3 ListNode
     var l3_node = &l3
@@ -16,15 +16,15 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) ListNode {
    
     var carry_over bool = false
     
-    for *l1_node != nil || *l2_node != nil || carry_over {
+    for l1_node != nil || l2_node != nil || carry_over {
         var sum int = 0
         
-        if *l1_node != nil{
-            sum += (*l1_node).Val
+        if l1_node != nil{
+            sum += l1_node.Val
         }
 
-        if *l2_node != nil{
-            sum += (*l2_node).Val
+        if l2_node != nil{
+            sum += l2_node.Val
         }
 
         if carry_over {
@@ -37,12 +37,12 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) ListNode {
             carry_over = true
         } 
 
-        if *l1_node != nil{
-            l1_node = &(*l1_node).Next
+        if l1_node != nil{
+            l1_node = l1_node.Next
         }
         
-        if *l2_node != nil{
-            l2_node = &(*l2_node).Next
+        if l2_node != nil{
+            l2_node = l2_node.Next
         }
         
         if !l3_empty{
