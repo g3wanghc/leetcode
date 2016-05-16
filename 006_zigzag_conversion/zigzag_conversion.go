33f86312efa0580d665ed9e119aedf04fa254923
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-05-16 13:38:37 -0400

    Spacing and indentation

--- 006_zigzag_conversion/zigzag_conversion.go
+++ 006_zigzag_conversion/zigzag_conversion.go
@@ -1,26 +1,26 @@
 func convert(s string, numRows int) string {
     if numRows == 1 {
-    	return s
+        return s
     }
 
     var converted_string string
 
     for r := 0; r < numRows; r++ {
-    	converted_string += filter_by_index(s, func(i int) bool {
-    			if i % ((numRows - 1) * 2) == r {
-    				return true
-    			}
-    			if (i + r) % ((numRows - 1) * 2) == 0 {
-    				return true
-    			}
-    			return false
-    		})
+        converted_string += filter_by_index(s, func(i int) bool {
+            if i % ((numRows - 1) * 2) == r {
+                return true
+            }
+            if (i + r) % ((numRows - 1) * 2) == 0 {
+                return true
+            }
+            return false
+        })
     }
 
     return converted_string
 }
 
-	
+    
 func filter_by_index(s string, f func(int) bool) string {
     var sf string
     for i, c := range s {
