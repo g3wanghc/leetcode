59175a2cc055d2d1b0d429daaefe50c12072b14e
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-05-16 10:06:04 -0400

    solution: 6. ZigZag Conversion

--- 006_zigzag_conversion/zigzag_conversion.go
+++ 006_zigzag_conversion/zigzag_conversion.go
@@ -1,9 +1,21 @@
 func convert(s string, numRows int) string {
+    if numRows == 1 {
+    	return s
+    }
+
     var converted_string string
- 
-    converted_string += filter_by_index(s, func(i int) bool {
-    			return i % ((numRows - 1) * 2) == 0
+
+    for r := 0; r < numRows; r++ {
+    	converted_string += filter_by_index(s, func(i int) bool {
+    			if i % ((numRows - 1) * 2) == r {
+    				return true
+    			}
+    			if (i + r) % ((numRows - 1) * 2) == 0 {
+    				return true
+    			}
+    			return false
     		})
+    }
 
     return converted_string
 }
