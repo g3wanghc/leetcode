fa2c5319fc91de472b11f6d69e0048b170a9b0c8
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-05-17 09:07:46 -0400

    004 Starter code

---
+++ 004_median_of_two_sorted_arrays/median_of_two_sorted_arrays.go
@@ -0,0 +1,6 @@
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+    while (len(nums1) > 1 || len(nums2) > 1 ) {
+        var nums1_median int
+        var nums2_median int
+    }
+}
